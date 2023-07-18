263178555753757d2b915c7ef756efeac58d6a51
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-07-18 13:00:38 -0300

    se realiza la configuracion inicial de los middlewares correspondientes para su uso

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
 
 const app = express();
 
@@ -6,3 +9,8 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("Estoy en el puerto " + app.get("port"));
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "/public")));
