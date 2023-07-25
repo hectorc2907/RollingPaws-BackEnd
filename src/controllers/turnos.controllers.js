3ff22a45f2ec360f7bb5df27dd6d4443e5bb042e
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-07-24 21:21:41 -0300

    se realiza el controlador para obtener todos los turnos guardados, se corrige el nombre de la funcion para obterner los turnos por id, ya que hacia referencia a todos los turnos y no al individual

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -1,13 +1,25 @@
 import Turnos from "../models/modeloTurnos";
 
-export const obtenerTurnos = async (req, res) => {
+export const obtenerTurno = async (req, res) => {
   try {
-    const turnos = await Turnos.findById(req.params.id);
-    res.status(200).json(turnos);
+    const turno = await Turnos.findById(req.params.id);
+    res.status(200).json(turno);
   } catch (error) {
     console.log(error);
     res.status(404).json({
-      mensaje: "No se encontraron los turnos guardados",
+      mensaje: "No se encontro el turno guardados",
     });
   }
 };
+
+export const obtenerTurnos = async (req,res) => {
+    try {
+        const turnos = await Turnos.find();
+        res.status(200).json(turnos);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({
+            mensaje:"No se encontraron los turnos guardados"
+        })
+    }
+}
