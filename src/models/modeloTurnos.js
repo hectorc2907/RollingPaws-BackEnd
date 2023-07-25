5dca53017a4e7447bdcc5bb47bedce238919aae4
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-07-24 21:11:42 -0300

    se realiza el modelo basico de los turnos que se cargaran con sus datos correspondientes y validaciones

---
+++ src/models/modeloTurnos.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const turnosSchema = new Schema({
+  nombreMascota: {
+    type: String,
+    minLength: 2,
+    maxLength: 20,
+    required: true,
+  },
+  nombreVeterinario: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+  fechaConsulta: {
+    type: Date,
+    required: true,
+  },
+  horaConsulta: {
+    type: String,
+    required: true,
+  },
+  detalleConsulta: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+});
+
+const Turnos = model("turnos",turnosSchema);
+
+export default Turnos;
