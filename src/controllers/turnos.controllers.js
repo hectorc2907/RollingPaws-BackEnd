7749808335cf3f97925e656a9399a69d8b872584
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-07-24 21:25:10 -0300

    se realiza el controlador para crear los turnos y guardarlos en la base de datos

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -12,14 +12,29 @@ export const obtenerTurno = async (req, res) => {
   }
 };
 
-export const obtenerTurnos = async (req,res) => {
-    try {
-        const turnos = await Turnos.find();
-        res.status(200).json(turnos);
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            mensaje:"No se encontraron los turnos guardados"
-        })
-    }
-}
+export const obtenerTurnos = async (req, res) => {
+  try {
+    const turnos = await Turnos.find();
+    res.status(200).json(turnos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se encontraron los turnos guardados",
+    });
+  }
+};
+
+export const crearTurnos = async (req, res) => {
+  try {
+    const turnoNuevo = new Turnos(req.body);
+    await turnoNuevo.save();
+    res.status(201).json({
+      mensaje: "El turno se creo correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al generar el turno",
+    });
+  }
+};
