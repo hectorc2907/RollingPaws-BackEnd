a5b1a8c9587e9d2d3e0f216bf6245aca56085f93
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-09-18 12:45:50 -0300

    se borran los log del controlador turnos

--- src/controllers/turnos.controllers.js
+++ src/controllers/turnos.controllers.js
@@ -5,7 +5,6 @@ export const obtenerTurno = async (req, res) => {
     const turno = await Turnos.findById(req.params.id);
     res.status(200).json(turno);
   } catch (error) {
-    console.log(error);
     res.status(404).json({
       mensaje: "No se encontro el turno guardados",
     });
@@ -17,7 +16,6 @@ export const obtenerTurnos = async (req, res) => {
     const turnos = await Turnos.find();
     res.status(200).json(turnos);
   } catch (error) {
-    console.log(error);
     res.status(404).json({
       mensaje: "No se encontraron los turnos guardados",
     });
@@ -32,7 +30,6 @@ export const crearTurnos = async (req, res) => {
       mensaje: "El turno se creo correctamente",
     });
   } catch (error) {
-    console.log(error);
     res.status(404).json({
       mensaje: "Error al generar el turno",
     });
@@ -46,7 +43,6 @@ export const bajaTurnos = async (req, res) => {
       mensaje: "El turno se elimino correctamente",
     });
   } catch (error) {
-    console.log(error);
     res.status(404).json({
       mensaje: "Error al eliminar el turno",
     });
@@ -60,7 +56,6 @@ export const editarDatosTurno = async (req, res) => {
       mensaje: "El turno se actualizo correctamente",
     });
   } catch (error) {
-    console.log(error);
     res.status(400).json({
       mensaje: "No se pudo actualizar los datos",
     });
